7c5c1a1099a79ae6912164d0d0b17b7b6a59a49d
Amir Mohammad <76002697+amirahmadi-ru@users.noreply.github.com>
2022-06-18 17:17:22 +0430

    Use UI and handle user clicks

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,29 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Calculator {
+public class Calculator implements ActionListener{
+	
 		JFrame frame;
+		JButton buttonNumber1;
+		JButton buttonNumber2;
+		JButton buttonNumber3;
+		JButton buttonAdd;
+		JButton buttonNumber4;
+		JButton buttonNumber5;
+		JButton buttonNumber6;
+		JButton buttonMulti;
+		JButton buttonNumber7;
+		JButton buttonNumber8;
+		JButton buttonNumber9;
+		JButton buttonDiv;
+		JButton buttonDecimal;
+		JButton buttonZero;
+		JButton buttonEqual;
+		JButton buttonMines;
+		JButton buttonDelete;
+		JButton buttonClear;
 		
 	public Calculator(){
 		
@@ -10,11 +31,11 @@ public class Calculator {
 	
 	public void setupAndShowMainFrame(){
 		frame = new JFrame("Calculator");
-        	frame.setSize(350,500);
-        	frame.setVisible(true);
-        	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        	frame.setResizable(false);
-        	frame.setLayout(null);
+        frame.setSize(350,500);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLayout(null);
         
 	}
 	
@@ -30,10 +51,16 @@ public class Calculator {
 		//--------------------------------------
 		// 1 - 2 - 3 - +
 		
-		JButton buttonNumber1 = new JButton("1");
-		JButton buttonNumber2 = new JButton("2");
-		JButton buttonNumber3 = new JButton("3");
-		JButton buttonAdd = new JButton("+");
+		buttonNumber1 = new JButton("1");
+		buttonNumber2 = new JButton("2");
+		buttonNumber3 = new JButton("3");
+		buttonAdd = new JButton("+");
+		
+		buttonNumber1.addActionListener(this);
+		buttonNumber2.addActionListener(this);
+		buttonNumber3.addActionListener(this);
+		buttonAdd.addActionListener(this);
+		
 
 		buttonNumber1.setBounds(40,100,50,40);
 		buttonNumber2.setBounds(110,100,50,40);
@@ -48,10 +75,15 @@ public class Calculator {
 		//--------------------------------------
 		// 4 - 5 - 6 - *
 		
-		JButton buttonNumber4 = new JButton("4");
-		JButton buttonNumber5 = new JButton("5");
-		JButton buttonNumber6 = new JButton("6");
-		JButton buttonMulti = new JButton("*");
+		buttonNumber4 = new JButton("4");
+		buttonNumber5 = new JButton("5");
+		buttonNumber6 = new JButton("6");
+		buttonMulti = new JButton("*");
+		
+		buttonNumber4.addActionListener(this);
+		buttonNumber5.addActionListener(this);
+		buttonNumber6.addActionListener(this);
+		buttonMulti.addActionListener(this);
 		
 		buttonNumber4.setBounds(40,170,50,40);
 		buttonNumber5.setBounds(110,170,50,40);
@@ -66,10 +98,15 @@ public class Calculator {
 		//--------------------------------------
 		// 7 - 8 - 9 - /
 		
-		JButton buttonNumber7 = new JButton("7");
-		JButton buttonNumber8 = new JButton("8");
-		JButton buttonNumber9 = new JButton("9");
-		JButton buttonDiv = new JButton("/");
+		buttonNumber7 = new JButton("7");
+		buttonNumber8 = new JButton("8");
+		buttonNumber9 = new JButton("9");
+		buttonDiv = new JButton("/");
+		
+		buttonNumber7.addActionListener(this);
+		buttonNumber8.addActionListener(this);
+		buttonNumber9.addActionListener(this);
+		buttonDiv.addActionListener(this);
 		
 		buttonNumber7.setBounds(40,240,50,40);
 		buttonNumber8.setBounds(110,240,50,40);
@@ -84,10 +121,15 @@ public class Calculator {
 		//--------------------------------------
 		// . - 0 - = - -
 		
-		JButton buttonDecimal = new JButton(".");
-		JButton buttonZero = new JButton("0");
-		JButton buttonEqual = new JButton("=");
-		JButton buttonMines = new JButton("-");
+		buttonDecimal = new JButton(".");
+		buttonZero = new JButton("0");
+		buttonEqual = new JButton("=");
+		buttonMines = new JButton("-");
+		
+		buttonDecimal.addActionListener(this);
+		buttonZero.addActionListener(this);
+		buttonEqual.addActionListener(this);
+		buttonMines.addActionListener(this);
 		
 		buttonDecimal.setBounds(40,310,50,40);
 		buttonZero.setBounds(110,310,50,40);
@@ -102,14 +144,91 @@ public class Calculator {
 		//--------------------------------------
 		// clear - delete
 		
-		JButton buttonDelete = new JButton("Delete");
-		JButton buttonClear = new JButton("Clear");
+		buttonDelete = new JButton("Delete");
+		buttonClear = new JButton("Clear");
+		
+		buttonDelete.addActionListener(this);
+		buttonClear.addActionListener(this);
 		
 		buttonDelete.setBounds(60,380,100,40);
 		buttonClear.setBounds(180,380,100,40);
 
 		frame.add(buttonDelete);
 		frame.add(buttonClear);
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == buttonNumber1){
+			System.out.println("button1");
+		}
+		if(e.getSource() == buttonNumber2){
+			System.out.println("button2");
+		}
+		if(e.getSource() == buttonNumber3){
+			System.out.println("button3");
+		}
+		if(e.getSource() == buttonAdd){
+			System.out.println("buttonadd");
+		}
+		
+		//--------------------------------------
+		
+		if(e.getSource() == buttonNumber4){
+			System.out.println("button4");
+		}
+		if(e.getSource() == buttonNumber5){
+			System.out.println("button5");
+		}
+		if(e.getSource() == buttonNumber6){
+			System.out.println("button6");
+		}
+		if(e.getSource() == buttonMulti){
+			System.out.println("buttonmulti");
+		}	
+		
+		//--------------------------------------
+		
+		if(e.getSource() == buttonNumber7){
+			System.out.println("button7");
+		}
+		if(e.getSource() == buttonNumber8){
+			System.out.println("button8");
+		}
+		if(e.getSource() == buttonNumber9){
+			System.out.println("button9");
+		}
+		if(e.getSource() == buttonDiv){
+			System.out.println("buttondiv");
+		}
+		
+		//--------------------------------------
+		
+		if(e.getSource() == buttonDecimal){
+			System.out.println("buttondecimal");
+		}
+		if(e.getSource() == buttonZero){
+			System.out.println("buttonzero");
+		}
+		if(e.getSource() == buttonEqual){
+			System.out.println("buttonequal");
+		}
+		if(e.getSource() ==  buttonMines){
+			System.out.println("buttonmines");
+		}
+		
+		//--------------------------------------
+		
+		if(e.getSource() == buttonDelete){
+			System.out.println("buttondelete");
+		}
+		if(e.getSource() == buttonClear){
+			System.out.println("buttonclear");
+		}
+		
 		
 	}
 }
